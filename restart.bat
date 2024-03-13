2c9d4803e7380731d511a4c8a6aff2c06e17268c
longfengpili <398745129@qq.com>
2024-03-13 10:40:28 +0800

    [优化]restart.bat

--- restart.bat
+++ restart.bat
@@ -7,4 +7,9 @@ docker rmi dify-worker
 docker rmi dify-api
 docker rmi dify-web
 
-docker-compose -f docker-compose-build.yml up
+docker-compose -f docker-compose-build.yml up -d
+
+touch ./volumes/app/storage/.gitkeep
+touch ./volumes/db/data/.gitkeep
+touch ./volumes/redis/data/.gitkeep
+touch ./volumes/weaviate/.gitkeep
